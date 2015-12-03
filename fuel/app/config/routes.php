1a24f2db46ee000d7b7d15eb2f2c76536abc1854
sharkpp <touroku@sharkpp.net>
2015-12-04 01:34:24 +0900

    update routes config

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -1,7 +1,9 @@
 <?php
 return array(
-	'_root_'  => 'welcome/index',  // The default route
-	'_404_'   => 'welcome/404',    // The main 404 route
-	
-	'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),
+	'_root_' => 'page/view',
+	'(create|edit|revision|list)' => 'page/$1',
+	'revision/(:num)' => 'page/view//$1',
+	'(:segment)/(create|edit|revision|delete)' => 'page/$2/$1',
+	'(:segment)/revision/(:num)' => 'page/view/$1/$2',
+	'(:segment)' => 'page/view/$1',
 );
