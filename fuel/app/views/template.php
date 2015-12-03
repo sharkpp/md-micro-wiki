3c4d2cb884be0ca26e27354c45a319685b88d749
sharkpp <touroku@sharkpp.net>
2015-12-04 01:37:44 +0900

    add menu and notify to view template

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -11,6 +11,20 @@
 <body>
 	<div class="container">
 		<div class="col-md-12">
+<?php $action = preg_match('!/(edit|revision|delete)(/[0-9]+)?!', '/' . Uri::string(), $match) ? $match[1] : ''; ?>
+			<div class="pull-right">
+				<?php echo Html::anchor('', 'Top'); ?>
+				|
+				<?php echo Html::anchor('list', 'List'); ?>
+				||
+				<?php echo '' == $action || 'list' == Uri::string() ? 'Read' : Html::anchor($name, 'Read'); ?>
+				|
+				<?php echo '' != $action || 'list' == Uri::string() ? 'Edit' : Html::anchor($name . '/edit', 'Edit'); ?>
+				|
+				<?php echo ('revision' == $action && !isset($match[2])) || ('revision' != $action && '' != $action) || 'list' == Uri::string() ? 'Revision' : Html::anchor($name . '/revision', 'Revision'); ?>
+				|
+				<?php echo '' != $action || '' == Uri::string() || 'list' == Uri::string() ? 'Delete' : Html::anchor($name . '/delete', 'Delete'); ?>
+			</div>
 			<h1><?php echo $title; ?></h1>
 			<hr>
 <?php if (Session::get_flash('success')): ?>
@@ -29,9 +43,15 @@
 				</p>
 			</div>
 <?php endif; ?>
+<?php if ('revision' == $action && isset($match[2])): ?>
+			<div class="alert alert-warning">
+				<p>This is an <?php echo Html::anchor($name . '/revision', 'old revision'); ?> of this page.</p>
+			</div>
+<?php endif; ?>
 		</div>
 		<div class="col-md-12">
 <?php echo $content; ?>
+			<hr>
 		</div>
 		<footer>
 			<p class="pull-right">Page rendered in {exec_time}s using {mem_usage}mb of memory.</p>
