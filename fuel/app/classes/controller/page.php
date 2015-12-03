9b32aa77b3fcda4a054e86d7a2d62352cdece719
sharkpp <touroku@sharkpp.net>
2015-12-04 01:45:38 +0900

    implement create and edit action

--- fuel/app/classes/controller/page.php
+++ fuel/app/classes/controller/page.php
@@ -32,11 +32,42 @@ class Controller_Page extends Controller_Template
 		$this->template->content = View::forge('page/revision', $data);
 	}
 
-	public function action_edit()
+	public function action_edit($name = '')
 	{
-		$data["subnav"] = array('edit'=> 'active' );
-		$this->template->title = 'Page &raquo; Edit';
-		$this->template->content = View::forge('page/edit', $data);
+		$page = Model_Page::get_by_title($name);
+		$new_page = ! $page;
+
+		if (Input::method() == 'POST') {
+			$val = Model_Page::validate($new_page ? 'create' : 'edit');
+			if ($val->run(array('title' => $name))) {
+				$page = $page ?: Model_Page::forge();
+				$page->title = $val->validated('title');
+				$page->body  = $val->validated('body');
+				$page->brief = $val->validated('brief');
+				if (!Input::post('preview')) {
+					if ($page->save()) {
+						Session::set_flash('success', $new_page ? 'Added page.' : 'Updated page.');
+						Response::redirect($name);
+					}
+					else {
+						Session::set_flash('error', $new_page ? 'Could not save page.' : 'Could not update page.');
+					}
+				}
+			}
+			else {
+				Session::set_flash('error', $val->error());
+			}
+			$this->template->set_global('body_html', Model_Page::parse_markdown(Input::post('body')), false);
+		}
+		else if ($page) {
+			$page->brief = '';
+		}
+
+		$this->template->set_global('page', $page, false);
+
+		$this->template->title = ($new_page ? 'Creating ' : 'Editing ') . (empty($name) ? '(top)' : $name);
+		$this->template->name = $name;
+		$this->template->content = View::forge('page/edit');
 	}
 
 	public function action_delete()
--- fuel/app/views/page/edit.php
+++ fuel/app/views/page/edit.php
@@ -1,9 +1,29 @@
-<ul class="nav nav-pills">
-	<li class='<?php echo Arr::get($subnav, "list" ); ?>'><?php echo Html::anchor('page/list','List');?></li>
-	<li class='<?php echo Arr::get($subnav, "view" ); ?>'><?php echo Html::anchor('page/view','View');?></li>
-	<li class='<?php echo Arr::get($subnav, "revision" ); ?>'><?php echo Html::anchor('page/revision','Revision');?></li>
-	<li class='<?php echo Arr::get($subnav, "edit" ); ?>'><?php echo Html::anchor('page/edit','Edit');?></li>
-	<li class='<?php echo Arr::get($subnav, "delete" ); ?>'><?php echo Html::anchor('page/delete','Delete');?></li>
+<?php if (isset($body_html)): ?>
+<div class="alert alert-info">
+	<p>This is only a preview; your changes have not yet been saved! → <a href="#form">Go to editing area</a></p>
+</div>
+<?php echo $body_html; ?>
+<hr>
+<?php endif ?>
+<?php echo Form::open(array("class"=>"form-horizontal")); ?>
 
-</ul>
-<p>Edit</p>
+	<fieldset id="form">
+		<div class="form-group">
+			<?php echo Form::label('Body', 'body', array('class'=>'control-label')); ?>
+
+				<?php echo Form::textarea('body', Input::post('body', isset($page) ? $page->body : ''), array('class' => 'col-md-8 form-control', 'rows' => 8, 'placeholder'=>'Body')); ?>
+
+		</div>
+		<div class="form-group">
+			<?php echo Form::label('Brief', 'brief', array('class'=>'control-label')); ?>
+
+				<?php echo Form::input('brief', Input::post('brief', isset($page) ? $page->brief : ''), array('class' => 'col-md-4 form-control', 'placeholder'=>'Brief')); ?>
+
+		</div>
+		<div class="form-group">
+			<?php echo Form::submit('submit', 'Save', array('class' => 'btn btn-primary')); ?>
+			<?php echo Form::submit('preview', 'Preview', array('class' => 'btn btn-normal')); ?>
+			<?php echo Html::anchor(isset($page) ? $page->title : '', 'Cancel', array('class' => 'btn btn-normal')); ?>
+		</div>
+	</fieldset>
+<?php echo Form::close(); ?>
